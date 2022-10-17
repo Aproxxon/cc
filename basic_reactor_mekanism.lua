72f6c1aed6c3af892e1ab517af638e5573d848fa
Yann Stepienik <azukaar@gmail.com>
2022-10-17 19:17:10 +0100

    Create basic_reactor_mekanism.lua

---
+++ basic_reactor_mekanism.lua
@@ -0,0 +1,36 @@
+local reactor = peripheral.wrap("bottom")
+
+while(true) do
+  local dam = reactor.getDamagePercent()
+  local cool = reactor.getHeatedCoolantFilledPercentage()
+  local waste = reactor.getWasteFilledPercentage()
+  
+  monitor.clear()
+  monitor.setCursorPos(1,1)
+  monitor.write("Damage: "..dam)
+  monitor.setCursorPos(1,2)
+  monitor.write("Coolant: "..cool)
+  monitor.setCursorPos(1,3)
+  monitor.write("Waste: "..waste)
+  monitor.setCursorPos(1,4)
+  
+  if(dam > 5) then
+    print("Stopped because of damage")
+    reactor.scram()
+  
+  
+  elseif(cool > 15) then
+    print("Stopped because of coolant")
+    reactor.scram()
+  
+  
+  elseif(waste > 15) then
+    print("Stopped because of waste")
+    reactor.scram()
+  
+  else
+     reactor.activate()
+  end
+  
+  sleep(1)
+end
